7a9f079c18e8d1eac825b24fddcebf08092179da
tuxor1337 <tuxor1337@web.de>
2013-04-24 22:26:34 +0300

    Backward compatibility

--- extension.js
+++ extension.js
@@ -1,28 +1,31 @@
-const PointerWatcher = imports.ui.pointerWatcher;
-const TrayPressure = imports.ui.main.layoutManager._trayPressure
 
-_myWatch = 0;
+const MessageTray = imports.ui.main.messageTray;
+const LayoutManager = imports.ui.main.layoutManager; 
+
+if("_trayPressure" in LayoutManager)
+    _myTrayPressure = LayoutManager._trayPressure;
+else _myTrayPressure = 0;
+_myDwell = 0;
 _myTrigger = 0;
 
 function init() { }
     
 function disable() {
-    if(_myWatch) {
-        PointerWatcher.getPointerWatcher()._watches = [_myWatch];
-        _myWatch = 0;
+    if(_myDwell) {
+        MessageTray._trayDwellTimeout = _myDwell;
+        _myDwell = 0;
     }
-    if(_myTrigger) {
-        TrayPressure._trigger = _myTrigger;
+    if(_myTrigger && _myTrayPressure) {
+        _myTrayPressure._trigger = _myTrigger;
         _myTrigger = 0;
     }
 }
    
 function enable() {
-    pw = PointerWatcher.getPointerWatcher();
-    if(pw._watches.length > 0) {
-        _myWatch = PointerWatcher.getPointerWatcher()._watches[0];
-        PointerWatcher.getPointerWatcher()._watches[0].remove();
+    _myDwell = MessageTray._trayDwellTimeout;
+    MessageTray._trayDwellTimeout = function() { return false; };
+    if(_myTrayPressure) {
+        _myTrigger = _myTrayPressure._trigger;
+        _myTrayPressure._trigger = function() { return false; };
     }
-    _myTrigger = TrayPressure._trigger;
-    TrayPressure._trigger = function() { return; };
 }
