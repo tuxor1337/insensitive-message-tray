8f522ecc17f9382b8701a4d9d265c70548dd9387
tuxor1337 <tuxor1337@web.de>
2013-05-28 22:53:11 +0200

    Much easier implementation for 3.8

--- extension.js
+++ extension.js
@@ -1,53 +1,28 @@
 
-const Main = imports.ui.main;
+const LayoutManager = imports.ui.main.layoutManager;
+const Shell = imports.gi.Shell;
+
 const MessageTray = imports.ui.main.messageTray;
-const LayoutManager = imports.ui.main.layoutManager; 
-
-if("_trayPressure" in LayoutManager)
-    _myTrayPressure = LayoutManager._trayPressure;
-else _myTrayPressure = 0;
 _myDwell = 0;
-_myTrigger = 0;
-
-_showEvent = 0;
-_hideEvent = 0;
 
 function init() { }
-    
-function disable() {
-    if(_showEvent) Main.overview.disconnect(_showEvent);
-    if(_hideEvent) Main.overview.disconnect(_hideEvent);
-    restore_dwell();
-    restore_trigger();
-}
-   
+
 function enable() {
-    suppress_trigger();
-    suppress_dwell();
-    _showEvent = Main.overview.connect('showing', restore_trigger);
-    _hideEvent = Main.overview.connect('hiding', suppress_trigger);
-}
-
-function suppress_trigger() {
-    if(_myTrayPressure) {
-        _myTrigger = _myTrayPressure._trigger;
-        _myTrayPressure._trigger = function() { return false; };
+    if("_trayPressure" in LayoutManager) {
+        LayoutManager._trayPressure._keybindingMode =
+            Shell.KeyBindingMode.OVERVIEW;
     }
-}
-
-function restore_trigger() {
-    if(_myTrigger && _myTrayPressure) {
-        _myTrayPressure._trigger = _myTrigger;
-        _myTrigger = 0;
-    }
-}
-
-function suppress_dwell() {
+        
     _myDwell = MessageTray._trayDwellTimeout;
     MessageTray._trayDwellTimeout = function() { return false; };
 }
 
-function restore_dwell() {
+function disable() {
+    if("_trayPressure" in LayoutManager) {
+        LayoutManager._trayPressure._keybindingMode = 
+            Shell.KeyBindingMode.NORMAL | Shell.KeyBindingMode.OVERVIEW;
+    }
+    
     if(_myDwell) {
         MessageTray._trayDwellTimeout = _myDwell;
         _myDwell = 0;
